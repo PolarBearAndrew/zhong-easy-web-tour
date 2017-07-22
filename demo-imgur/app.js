f54e9beb9c75b633d6bd65ac679e1052104e52c8
PolarBearAndrew <chenpoanandrew@gmail.com>
2017-07-22 22:52:10 +0800

    refac,  extract methods

--- demo-imgur/app.js
+++ demo-imgur/app.js
@@ -13,47 +13,53 @@ const app = new Vue({
     images: []
   },
   methods: {
-    handleFileChange: function(e) {
-      const files = e.target.files;
-
-      if (files.length === 0) {
-        return;
-      }
-
-      const handleNewImgur = (res) => {
-        that.images.push({
-          src: res.data.link
-        });
-      };
-
-      for (let i = 0; i < files.length; i++) {
-        const file = files[i];
-
-        if (this.isHandleType(file.type)) {
-          this.uploadImage(file, handleNewImgur);
-        } else {
-          alert('is not handle handle type, file =', file);
-        }
-      }
-    },
-    isHandleType: function(type) {
-      return _HANDLE_TYPE.indexOf(type) !== -1;
-    },
-    uploadImage: function(file, callback) {
-      const formData = new FormData();
-      formData.append('image', file);
-
-      const options = {
-        method: 'POST',
-        headers: {
-          Authorization: config.imgurToken
-        },
-        body: formData
-      };
-
-      fetch(_IMGUR_UPLOAD_CDN, options)
-        .then(res => res.json())
-        .then(callback);
-    }
+    handleFileChange: handleFileChange,
+    isHandleType: isHandleType,
+    uploadImage: uploadImage
   }
 });
+
+function handleFileChange(e) {
+  const files = e.target.files;
+
+  if (files.length === 0) {
+    return;
+  }
+
+  const handleNewImgur = (res) => {
+    that.images.push({
+      src: res.data.link
+    });
+  };
+
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i];
+
+    if (this.isHandleType(file.type)) {
+      this.uploadImage(file, handleNewImgur);
+    } else {
+      alert('is not handle handle type, file =', file);
+    }
+  }
+}
+
+function isHandleType(type) {
+  return _HANDLE_TYPE.indexOf(type) !== -1;
+}
+
+function uploadImage(file, callback) {
+  const formData = new FormData();
+  formData.append('image', file);
+
+  const options = {
+    method: 'POST',
+    headers: {
+      Authorization: config.imgurToken
+    },
+    body: formData
+  };
+
+  fetch(_IMGUR_UPLOAD_CDN, options)
+    .then(res => res.json())
+    .then(callback);
+}
